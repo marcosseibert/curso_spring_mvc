837367a70e240b6b6dabf015d81480e918b70002
seibert <marcosantonioseibert@gmail.com>
2018-07-25 21:15:33 -0300

    Codigo do projeto Spring MVC da Algawors

---
+++ src/main/java/com/seibert/cobranca/repository/Titulos.java
@@ -0,0 +1,9 @@
+package com.seibert.cobranca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.seibert.cobranca.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long> {
+
+}
